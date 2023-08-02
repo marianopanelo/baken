c5b97f471b9e4033914f3a33500323469910c574
mariano <marianoapanelo@gmail.com>
2023-08-02 20:37:52 -0300

    cuarto entregable,falta

--- scr/app.js
+++ scr/app.js
@@ -1,20 +1,42 @@
 import express from "express"
 import productosroutes from "./routes/productos.routes.js"
 import carritoroutes from "./routes/carrito.routes.js"
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
+import { Server } from "socket.io";
+import caritoManager from "./caritomanager.js"
 
 const app = express ()
 const PORT = 8080
 
+const carito = new caritoManager ()
+let productoCarritoNombre = await carito.tranformarCarritoJSON()
+
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+app.use(express.static(__dirname + "/public"))
 
 app.use('/api/productos', productosroutes);
 app.use('/api/carrito', carritoroutes);
 
 
-app.listen(PORT, ()=>{
+const abrirServer = app.listen(PORT, ()=>{
 console.log("server abierto");
 })
 
 
+/*envio el carrito desde socket, pero lo puedo enviar por log , no se como enviarlo por pantalla o parametro hasta la clase del miercoles 31/JUL */
+const socketServer = new Server (abrirServer)
+
+socketServer.on("connection" , socket =>{
+    console.log("cliente conectado");
+
+
+    socketServer.emit("evento_para_todos", productoCarritoNombre)
+})
--- scr/routes/carrito.routes.js
+++ scr/routes/carrito.routes.js
@@ -1,26 +1,30 @@
 import caritoManager from "../caritomanager.js"
 import producManager from "../ProductManager.js"
-
 import { Router, response } from "express";
 
 
 const router = Router()
 
 const carito = new caritoManager ()
-//const carritoTranformado = await carito.tranformarCarritoJSON()
-//console.log(carritoTranformado);
+
 
 const productos = new producManager()
-
-
+let productosNombre = await productos.tranformarJSON()
+let productoCarritoNombre = await carito.tranformarCarritoJSON()
 /* todos los productos del carrito entiendo q en esto solo tenia q mostrar esto , no entendi bien en la diapo, pero supongo q es eso
 cree algunos productos en carrito para q tenga algo , que se importan desde indexcarrito ... no entendi bien lo de :cid a que se referia  */
-router.get ("/" , async (req , res)  =>{
+/*router.get ("/" , async (req , res)  =>{
     res.send (await carito.tranformarCarritoJSON())      
+}    
+) dejo esto comentado */
+
+
+
+router.get ("/" , async (req , res)  =>{
+    res.render ("home" , {productoCarritoNombre})
 }    
 )
 
-/*entiendoq  tengo q crear un post que me cree otros productos , pero solo con el id y si se repite me tire que ya se creo algo con ese id*/
 router.post("/addProductosNoCarrito" , async(req,res)=>{
     let nuevoProducto = req.body
     if (await carito.buscarProductoEnCarritoPorId(nuevoProducto.id) == "producto no encontrado con ese id") {
@@ -32,17 +36,24 @@ router.post("/addProductosNoCarrito" , async(req,res)=>{
     }
 })
 
-/*agregar un productos al carrito ya existente en productos */
 router.post("/addProductoCarrito/:id", async(req,res)=>{
     let productoId = parseInt(req.params.id)
     console.log(await productos.buscarProductoPorId(productoId));
 
     if (await productos.buscarProductoPorId(productoId) == "producto no encontrado con ese id") {
-        return res.status(400).send({status : "error" ,msj: "no existe producto con ese id en productos"})
+        return res.status(400).send({status : "error" ,msj: "no existe producto con ese id"})
     }else{
     await carito.agregarProductoCarrito(productoId)
     res.send("el producto con el id : " + productoId + " fue agregado" )
     }
 })
+
+
+/*solo handlebars */
+router.get ("/realTimeProducts" , async (req , res)  =>{
+    res.render ("realTimeProducts", {productoCarritoNombre} )
+}    
+)
+
 
 export default router;
